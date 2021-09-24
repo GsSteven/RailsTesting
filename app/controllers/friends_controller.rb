e965eb03d5d6d10e30f5238b435fe5c83b068808
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-09-24 14:48:12 -0400

    account association with friends list and redirect to sign-in if not logged in

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,11 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /friends or /friends.json
   def index
+    redirect_to new_user_session_url if current_user.nil?
     @friends = Friend.all
   end
 
@@ -12,7 +15,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    #@friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -21,7 +25,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    #@friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -54,6 +59,11 @@ class FriendsController < ApplicationController
       format.html { redirect_to friends_url, notice: "Friend was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friend_path, notice: "Not Authorized to Edit" if @friend.nil?
   end
 
   private
